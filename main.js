b3f2f29e23f2eca4ccd0833ad1ef28300fe6eb92
manognachalla <141226765+manognachalla@users.noreply.github.com>
2024-05-21 19:52:39 +0530

    add main files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+// switch for setting
+function settingtoggle(){
+  document.getElementById("setting-container").classList.toggle('settingactivate');
+  document.getElementById("visualmodetogglebuttoncontainer").classList.toggle('visualmodeshow');
+}
+
+  function visualmode(){
+    document.body.classList.toggle('light-mode');
+    var elements = document.querySelectorAll('.needtobeinvert');
+    elements.forEach(function(element) {
+        element.classList.toggle('invertapplied');
+    });
+
+  }
+let emptyArea = document.getElementById("emptyarea");
+
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.navbar .navbar-tabs .navbar-tabs-ul li');
+const mobilenavLi = document.querySelectorAll('.mobiletogglemenu .mobile-navbar-tabs-ul li');
+
+window.addEventListener('scroll', ()=>{
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if(pageYOffset >= (sectionTop - 200)){
+      current = section.getAttribute('id');
+    }
+  })
+
+  mobilenavLi.forEach( li => {
+    li.classList.remove('activeThismobiletab');
+    if(li.classList.contains(current)){
+      li.classList.add('activeThismobiletab')
+    }
+  })
+navLi.forEach( li => {
+  li.classList.remove('activeThistab');
+  if(li.classList.contains(current)){
+    li.classList.add('activeThistab')
+  }
+})
+})
+console.log('%c Designed and Developed by Manogna&Yeshwanth ', 'background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;');
+
+
+
+let mybutton = document.getElementById("backtotopbutton");
+window.onscroll = function(){
+  scrollFunction()
+};
+
+function scrollFunction(){
+  if(document.body.scrollTop > 400 || document.documentElement.scrollTop > 400)
+  {
+    mybutton.style.display = "block";
+  }
+   else{
+      mybutton.style.display = "none";
+     
+      }
+}
+
+function scrolltoTopfunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener("contextmenu", function(e){
+  if (e.target.nodeName === "IMG") {
+      e.preventDefault();
+  }
+}, false);
+
+
+
+const windowResize = (event) => {
+    mouseXEndPoint = window.innerWidth;
+    mouseYEndPoint = window.innerHeight;
+    mouseXRange = mouseXEndPoint - mouseXStartPoint;
+}
+
+
+window.addEventListener('mousemove', mouseMove);
+window.addEventListener('resize', windowResize);
